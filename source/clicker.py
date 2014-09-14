1e0c204ac97d7b3f2deea2239915fe25038008a1
arandomone <arandom11111@gmail.com>
2014-09-14 18:37:55 -0400

    Ver 2.1
    +Added another unimplemented building (red bred)
    +Added engis
    +Added more doxes
    +String concatenation
    *Updated function names
    *Moved increaseval outside the app class
    *Tried to make all the lines under 80 columns without breaking
    everything

--- source/clicker.py
+++ source/clicker.py
@@ -19,7 +19,7 @@ class Building:
 TELE1 = Building(10, 0.1)
 TELE2 = Building(100, 10)
 TELE3 = Building(1000, 1000)
-#Engineers
+#ENGINEERS
 ENGI = Building(100, 1)
 MECHENGI = Building(1000, 100)
 UBERENGI = Building(10000, 10000)
@@ -31,10 +31,14 @@ SERB32 = Building(100000, 100000)
 GAMEL = Building(10000, 100)
 GAMEM = Building(100000, 10000)
 GAMEH = Building(1000000, 1000000)
+#RED BRED
+#2op4u
+RBRED = Building(100000000, 100000000)
 #ALL of the stuff above is here
 BUILDLIST = [TELE1, TELE2, TELE3, ENGI, MECHENGI, UBERENGI, SERB24, SERB24,
-             SERB32, GAMEL, GAMEM, GAMEH]
+             SERB32, GAMEL, GAMEM, GAMEH, RBRED]
 class Application(tk.Frame):
+    """The application used in bread teleportation"""
     def __init__(self, master=None):
         """To get this all set up"""
         tk.Frame.__init__(self, master)
@@ -43,12 +47,7 @@ class Application(tk.Frame):
         self.bread = 0
         self.bps = 0
         self.bislist = []
-        self.createWidgets()
-
-    def increaseval(self, name, val):
-        """Increasing values (Expect this to be replaced)"""
-        #DO NOT use this for buying buildings
-        exec("self." + name + " += " + str(val))
+        self.createwidgets()
 
     def buybuilding(self, location):
         """The correct way of buying a building"""
@@ -56,9 +55,9 @@ class Application(tk.Frame):
         if self.bread >= building.cost:
             self.bread -= building.cost
             building.amt += 1
-            building.cost *= 1.5 
+            building.cost = round(building.cost * 1.5)
             self.buildings[location] = building
-        
+
     def recalcbps(self):
         """Recalculates the bps"""
         tempbps = 0
@@ -67,66 +66,91 @@ class Application(tk.Frame):
         self.bps = tempbps
 
     def addtoapp(self, name, text, tktype="Label", command=None, disabled=False,
-                 grid = ""):
-        """The easy(?) way to add something to the app"""
-        exec("self." + name + " = tk." + tktype + "(self)")
-        exec("self." + name + "['text'] = text")
+                 grid=""):
+        """The easy(?) way to add something to the APP"""
+        exec("self.%s = tk.%s(self)" % (name, tktype))
+        exec("self.%s['text'] = text" % (name))
         if not command == None:
-            exec("self." + name + "['command'] = " + command)
+            exec("self.%s['command'] = %s" % (name, command))
         if not disabled == False:
-            exec("self." + name + "['state'] = tk.DISABLED")
-        exec("self." + name + ".grid(" + grid + ")")
+            exec("self.%s['state'] = tk.Disabled" % (name))
+        exec("self.%s.grid(%s)" % (name, grid))
         self.bislist.append([name, text])
 
-    def createWidgets(self):
+    def createwidgets(self):
         """Create the buttons and labels"""
         #GET BREAD BUTTON
         self.addtoapp("add_bread", "Teleport bread", "Button",
-                      "lambda: app.increaseval('bread', 1)")
+                      "lambda: increaseval('bread', 1)")
         #BREAD COUNT
         self.addtoapp("breadcount", "str(self.bread)")
         #BPS COUNT
         self.addtoapp("bpscount", "str(self.bps)")
         ##TELEPORTERS##
         self.teleframe = tk.Frame()
-        self.teleframe.grid(row=1)
+        self.teleframe.grid(row=1, column=0)
         #BUY TELE1 BUTTON
         self.addtoapp("teleframe.buy_tele1",
-                      "Buy a level 1 Teleporter (cost: " + str(self.buildings[0].cost) + " )",
-                      "Button", "lambda: app.buybuilding(0)")
+                      "Buy a level 1 Teleporter (cost: %s)" % (str(self.buildings[0].cost)),
+                      "Button", "lambda: APP.buybuilding(0)")
         #BUY TELE2 BUTTON
         self.addtoapp("teleframe.buy_tele2",
-                      "Buy a level 2 Teleporter (cost: " + str(self.buildings[1].cost) + " )",
-                      "Button", "lambda: app.buybuilding(1)" #True
+                      "Buy a level 2 Teleporter (cost: %s)" % (str(self.buildings[1].cost)),
+                      "Button", "lambda: APP.buybuilding(1)" #True
                       )
         #BUY TELE3 BUTTON
         self.addtoapp("teleframe.buy_tele3",
-                      "Buy a level 3 Teleporter (cost: " + str(self.buildings[2].cost) + " )",
-                      "Button", "lambda: app.buybuilding(2)" #True
+                      "Buy a level 3 Teleporter (cost: %s)" % (str(self.buildings[2].cost)),
+                      "Button", "lambda: APP.buybuilding(2)" #True
                       )
-        
-    def updateWidgets(self):
+        ##ENGIS##
+        self.engiframe = tk.Frame()
+        self.teleframe.grid(row=1, column=1)
+        #BUY ENGI BUTTON
+        self.addtoapp("engiframe.buy_engi",
+                      "Buy an engineer (cost: %s)" % (str(self.buildings[3].cost)),
+                      "Button", "lambda: APP.buybuilding(3)")
+        #BUY MECHA ENGI BUTTON
+        self.addtoapp("engiframe.buy_tobengi",
+                      "Buy a robotic engineer (cost: %s)" % (str(self.buildings[4].cost)),
+                      "Button", "lambda: APP.buybuilding(4)" #True
+                      )
+        self.addtoapp("engiframe.buy_ubengi",
+                      "Buy an ubercharged engineer (cost: %s)" % (str(self.buildings[5].cost)),
+                      "Button", "lambda: APPbuybuilding(5)" #True
+                      )
+
+    def updatewidgets(self):
         """It looks easy but I want to try and put a for loop in here"""
         self.breadcount["text"] = str(int(self.bread))
-        self.teleframe.buy_tele1["text"] = "Buy a level 1 Teleporter (cost: " + str(round(self.buildings[0].cost)) + " )"
-        self.teleframe.buy_tele2["text"] = "Buy a level 2 Teleporter (cost: " + str(round(self.buildings[1].cost)) + " )"
-        self.teleframe.buy_tele3["text"] = "Buy a level 3 Teleporter (cost: " + str(round(self.buildings[2].cost)) + " )"
+        self.teleframe.buy_tele1["text"] = "Buy a level 1 Teleporter (cost: %s)" % (str(self.buildings[0].cost))
+        self.teleframe.buy_tele2["text"] = "Buy a level 2 Teleporter (cost: %s)" % (str(self.buildings[1].cost))
+        self.teleframe.buy_tele3["text"] = "Buy a level 3 Teleporter (cost: %s)" % (str(self.buildings[2].cost))
+        self.engiframe.buy_engi["text"] = "Buy an engineer (cost: %s)" % (str(self.buildings[3].cost))
+        self.engiframe.buy_tobengi["text"] = "Buy a robotic engineer (cost: %s)" % (str(self.buildings[4].cost))
+        self.engiframe.buy_ubengi["text"] = "Buy an ubercharged engineer (cost: %s)" % (str(self.buildings[5].cost)),
         self.bpscount["text"] = str(self.bps)
 
 
-root = tk.Tk()
-app = Application(master=root)
+def increaseval(name, val):
+    """Increasing values from tkinter (Expect this to be replaced)"""
+    #DO NOT use this for buying buildings
+    exec("APP.%s += val" % (name))
+
+ROOT = tk.Tk()
+APP = Application(master=ROOT)
 
 def pigloop1():
     """This loop piggybacks on to tkinter's main loop"""
-    app.bread += app.bps
-    app.after(1000, pigloop1)
+    APP.bread += APP.bps
+    APP.after(1000, pigloop1)
 
 def pigloop2():
-    app.recalcbps()
-    app.updateWidgets()
-    app.after(100, pigloop2)
+    """This also piggybacks but is used for updating numbers"""
+    APP.recalcbps()
+    APP.updatewidgets()
+    APP.after(100, pigloop2)
 
-app.after(0, pigloop1)
-app.after(0, pigloop2)
-app.mainloop()
+APP.after(0, pigloop1)
+APP.after(0, pigloop2)
+APP.mainloop()
