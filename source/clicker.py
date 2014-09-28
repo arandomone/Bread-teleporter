40ab287c39feaf49f00d5297585707e350e3a68f
arandomone <arandom11111@gmail.com>
2014-09-28 15:27:42 -0400

    Ver 0.2.5
    *Renamed Goals to achievements
    +Added broken upgrades
    -Removed 5 buildings (The clone ones)

--- source/clicker.py
+++ source/clicker.py
@@ -12,8 +12,8 @@ class Building:
         #Amount
         self.amt = 0
 
-class Goal:
-    """For achievements"""
+class Achievement:
+    """For achievements dummy"""
     def __init__(self, text, task, prize=lambda: None):
         self.text = text
         self.task = task
@@ -21,41 +21,54 @@ class Goal:
         self.prize = prize
         self.gotten = False
 
+class Upgrade:
+    """For upgrades"""
+    def __init__(self, text, cond, cost, effect):
+        self.text = text
+        self.cond = cond
+        self.cost = cost
+        self.effect = effect
+        self.bought = False
+
 #BUILDINGS:
 #Going by base 10 values until I think of better ones
 #Level 1,2, and 3 teleporters
-TELE1 = Building(10, 0.1)
-TELE2 = Building(100, 10)
-TELE3 = Building(1000, 1000)
+TELE = Building(10, 0.1)
 #ENGINEERS
 ENGI = Building(100, 1)
-MECHENGI = Building(1000, 100)
-UBERENGI = Building(10000, 10000)
 #LASTS
 SERB = Building(100000, 1000)
 GAME = Building(1000000, 100000)
 #2op4u
 RBRED = Building(100000000, 100000000)
 #ALL of the stuff above is here
-BUILDLIST = [TELE1, TELE2, TELE3, ENGI, MECHENGI, UBERENGI, SERB, GAME, RBRED]
+BUILDLIST = [TELE, ENGI, SERB, GAME, RBRED]
 
 #ACHIEVEMENTS:
-ACH1 = Goal("'It begins!'", lambda: APP.totalbread == 1)
-ACH2 = Goal("'Actually playing!'", lambda: APP.totalbread == 10)
-ACH3 = Goal("'Dont stop now!'", lambda: APP.totalbread == 100)
-ACH4 = Goal("'Love!'", lambda: APP.bps == 1)
-ACH5 = Goal("'And!'", lambda: APP.bps == 10)
-ACH6 = Goal("'War!'", lambda: APP.bps == 100)
-ACH7 = Goal("'Update!'", lambda: APP.bps == 1000)
-ACH8 = Goal("'Confirmed!'", lambda: APP.bps == 10000)
-ACH9 = Goal("'Released!'", lambda: APP.bps == 100000)
-ACH10 = Goal("'Keep going!'", lambda: APP.clicks == 10)
-ACH11 = Goal("'Your doing great!'", lambda: APP.clicks == 100)
-ACH12 = Goal("'Its time to stop clicking!'", lambda: APP.clicks == 1000)
-ACH13 = Goal("'Autoclicker!'", lambda: APP.clicks == 10000)
+ACH1 = Achievement("'It begins!'", lambda: APP.totalbread == 1)
+ACH2 = Achievement("'Actually playing!'", lambda: APP.totalbread == 10)
+ACH3 = Achievement("'Dont stop now!'", lambda: APP.totalbread == 100)
+ACH4 = Achievement("'Love!'", lambda: APP.bps == 1)
+ACH5 = Achievement("'And!'", lambda: APP.bps == 10)
+ACH6 = Achievement("'War!'", lambda: APP.bps == 100)
+ACH7 = Achievement("'Update!'", lambda: APP.bps == 1000)
+ACH8 = Achievement("'Confirmed!'", lambda: APP.bps == 10000)
+ACH9 = Achievement("'Released!'", lambda: APP.bps == 100000)
+ACH10 = Achievement("'Keep going!'", lambda: APP.clicks == 10)
+ACH11 = Achievement("'Your doing great!'", lambda: APP.clicks == 100)
+ACH12 = Achievement("'Its time to stop clicking!'", lambda: APP.clicks == 1000)
+ACH13 = Achievement("'Autoclicker!'", lambda: APP.clicks == 10000)
 
 ACHLIST = [ACH1, ACH2, ACH3, ACH4, ACH5, ACH6, ACH7, ACH8, ACH9, ACH10, ACH11,
            ACH12, ACH13]
+#UPGRADES:
+UP1 = Upgrade("Level 2 teleporters", lambda: APP.buildings[0].amt == 1, 100,
+              "APP.buildings[0].addbps *= 2")
+UP2 = Upgrade("Level 3 teleporters", lambda: APP.buildings[0].amt == 50, 10000,
+              "APP.buildings[0].addbps *= 4")
+UP3 = Upgrade("Level 4 teleporters", lambda: APP.buildings[0].amt == 100, 100000,
+              "APP.buildings[0].addbps *= 6")
+UPLIST = [UP1, UP2, UP3]
 class Application(tk.Frame):
     """The application used in bread teleportation"""
     def __init__(self, master=None):
@@ -68,6 +81,7 @@ class Application(tk.Frame):
         self.bps = 0
         self.clicks = 0
         self.achievements = ACHLIST
+        self.upgrades = UPLIST
         self.bislist = []
         self.createwidgets()
 
@@ -104,51 +118,28 @@ class Application(tk.Frame):
         self.addtoapp("breadcount", "'You have %i breads' % (APP.bread)", grid="row=1, column=0")
         #BPS COUNT
         self.addtoapp("bpscount", "'You are making %s bread per second' % (str(APP.bps))", grid="row=2, column=0")
-        ##TELEPORTERS##
-        self.teleframe = tk.Frame()
-        self.teleframe.grid(row=3, column=0)
-        #BUY TELE1 BUTTON
-        self.addtoapp("teleframe.buy_tele1",
-                      "'Buy a level 1 Teleporter (cost: %i)' % (APP.buildings[0].cost)",
+        #BUY TELE BUTTON
+        self.addtoapp("buy_tele",
+                      "'Buy a Teleporter (cost: %i)' % (APP.buildings[0].cost)",
                       "Button", "lambda: APP.buybuilding(0)")
-        #BUY TELE2 BUTTON
-        self.addtoapp("teleframe.buy_tele2",
-                      "'Buy a level 2 Teleporter (cost: %i)' % (APP.buildings[1].cost)",
-                      "Button", "lambda: APP.buybuilding(1)")
-        #BUY TELE3 BUTTON
-        self.addtoapp("teleframe.buy_tele3",
-                      "'Buy a level 3 Teleporter (cost: %i)' % (APP.buildings[2].cost)",
-                      "Button", "lambda: APP.buybuilding(2)")
-        ##ENGIS##
-        self.engiframe = tk.Frame()
-        self.teleframe.grid(row=3, column=1)
         #BUY ENGI BUTTON
-        self.addtoapp("engiframe.buy_engi",
-                      "'Buy an engineer (cost: %i)' % (APP.buildings[3].cost)",
-                      "Button", "lambda: APP.buybuilding(3)")
-        #BUY MECHA ENGI BUTTON
-        self.addtoapp("engiframe.buy_tobengi",
-                      "'Buy a robotic engineer (cost: %i)' % (APP.buildings[4].cost)",
-                      "Button", "lambda: APP.buybuilding(4)")
-        #BUY UBER ENGI BUTTON
-        self.addtoapp("engiframe.buy_ubengi",
-                      "'Buy an ubercharged engineer (cost: %i)' % (APP.buildings[5].cost)",
-                      "Button", "lambda: APP.buybuilding(5)")
-        ##LASTS###
-        self.lastframe = tk.Frame()
-        self.lastframe.grid(row=3, column=2)
+        self.addtoapp("buy_engi",
+                      "'Buy an engineer (cost: %i)' % (APP.buildings[1].cost)",
+                      "Button", "lambda: APP.buybuilding(1)")
         #BUY SERVER
-        self.addtoapp("lastframe.buy_serb",
-                      "'Buy your own server (cost: %i)' % (APP.buildings[6].cost)",
-                      "Button", "lambda: APP.buybuilding(6)")
+        self.addtoapp("buy_serb",
+                      "'Buy your own server (cost: %i)' % (APP.buildings[2].cost)",
+                      "Button", "lambda: APP.buybuilding(2)")
         #BUY GAME
-        self.addtoapp("lastframe.buy_game",
-                      "'Buy a game making company (cost: %i)' % (APP.buildings[7].cost)",
-                      "Button", "lambda: APP.buybuilding(7)")
+        self.addtoapp("buy_game",
+                      "'Buy a game making company (cost: %i)' % (APP.buildings[3].cost)",
+                      "Button", "lambda: APP.buybuilding(3)")
         #BUY RED BRED
-        self.addtoapp("lastframe.buy_rbred",
-                      "'Buy Red Bread (cost: %i)' % (APP.buildings[8].cost)",
-                      "Button", "lambda: APP.buybuilding(8)")
+        self.addtoapp("buy_rbred",
+                      "'Buy Red Bread (cost: %i)' % (APP.buildings[4].cost)",
+                      "Button", "lambda: APP.buybuilding(4)")
+        self.upframe = tk.Frame()
+        self.upframe.grid()
 
 
 def increasebread(val=1, click=False):
@@ -183,6 +174,14 @@ def pigloop2():
             goal.prize()
             print("You have achieved the %s achievement!" % (goal.text))
             goal.gotten = True
+    #UPGRADE UPDATE
+    #upshown = 0
+    #for up in APP.upgrades:
+    #    if up.cond() and not up.bought:
+    #        exec("APP.upframe.up%i = tk.Button" % (upshown))
+    #        exec("APP.upframe.up%i['text'] = up.text" % (upshown))
+    #        exec("APP.upframe.up%i['command'] = up.effect" % (upshown))
+    #        upshown += 1
     APP.after(100, pigloop2)
 
 APP.after(0, pigloop1)
